3d3797b5dd709992a31878ade047b93f1b0f4c38
vcamarggo <vinicamarggo@hotmail.com>
2018-11-09 08:28:21 -0200

    From gif to gif

--- gif_coloring.py
+++ gif_coloring.py
@@ -1,12 +1,11 @@
 import numpy as np
 import cv2
 import imageio
-import glob
-import natsort
+import os
+from PIL import Image
 
 INIT_COLOR_HUE = 0
 END_COLOR_HUE = 180
-FRAMES =60
 
 HEIGHT= 128
 WIDTH= HEIGHT*2 #O dobro pq vai dar append na altura com a segunda imagem
@@ -14,10 +13,22 @@ WIDTH= HEIGHT*2 #O dobro pq vai dar append na altura com a segunda imagem
 MIN_VALUE=0
 MAX_VALUE=255
 
-for idx in range(FRAMES):
+gif=[]
+
+frame = Image.open('in.gif')
+nframes = 0
+while frame:
+	frame.save( '%s/%s-%s.png' % ('input-img/', os.path.basename('input'), nframes ))
+	nframes += 1
+	try:
+		frame.seek( nframes )
+	except EOFError:
+		break;
+
+for idx in range(nframes):
     
-    current_hue = ((END_COLOR_HUE-INIT_COLOR_HUE)/FRAMES)*idx
-    
+    current_hue = ((END_COLOR_HUE-INIT_COLOR_HUE)/nframes)*idx
+        
     h= np.repeat(np.repeat(current_hue, HEIGHT),WIDTH).reshape(HEIGHT,-1)
     s= np.repeat(np.array(np.arange(MIN_VALUE, MAX_VALUE))[::-2],WIDTH).reshape(HEIGHT,-1)
     v= np.repeat(np.repeat(MAX_VALUE, HEIGHT),WIDTH).reshape(HEIGHT,-1)
@@ -33,22 +44,19 @@ for idx in range(FRAMES):
     palette = np.append(black_color,color_white, axis=0)
     bgr_palette = cv2.cvtColor(palette,cv2.COLOR_HSV2BGR)
     
-    img = cv2.imread('img2.png',0)
+    img = cv2.imread('input-img/input-'+str(idx)+'.png',0)
     
-    final_img = cv2.imread('img2.png',1)
+    final_img = cv2.imread('input-img/input-'+str(idx)+'.png',1)
     
     bgr_indexes = bgr_palette[::,0]
     
     for i in range(len(img)):
         for j in range(len(img[i])):
             final_img[i][j] = bgr_indexes[img[i][j]]
-            
-    cv2.imwrite('result/img-'+str(idx)+'-colored.jpg', final_img)
+			
+    gif.append(cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB)) 
      
-images=[]
-for filename in natsorted(glob.iglob('result/*.jpg'), key=lambda y: y.lower()):
-    images.append(imageio.imread(filename))
-imageio.mimsave('result/movie.gif', images, duration=0.2)
+imageio.mimsave('out.gif', gif, duration=0.1)
 
 def write_palettes():    
     for i in range(180):
